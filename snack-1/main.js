f5b1d0c0af8923e095694b8f84d0c5ab92d0e0b5
LeleGuidi <emanuele.guidi98@outlook.it>
2022-05-05 12:42:43 +0200

    Corretto Snack-1

--- snack-1/main.js
+++ snack-1/main.js
@@ -1,8 +1,17 @@
+//Creare un'array vuoto
 let numbers = [];
+let flag = true 
 let sum = 0
-do {
+//Chiedere all'utente un numero e inserirlo nell'array fino a quando la loro somma è inferiore a 50
+while (flag) {
     const number = Number(prompt("Inserisci un numero"))
-    numbers.push(number)
-    sum += number
-} while (sum < 50)
+    //Se la somma più il numero è inferiore a 50 allora il numero dell'utente va inserito alla somma e all'array
+    if ((sum + number) < 50) {
+        sum += number 
+        numbers.push(number)
+    //Altrimenti il flag diventa falso e si esce dal ciclo
+    } else {
+        flag = false
+    }
+}
 console.log(numbers, sum)
